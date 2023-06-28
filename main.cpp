6f24ebcf4220bd53a874cae603073c3e57c1ffe0
Leon <rguarne1@gmail.com>
2023-06-27 20:58:49 -0500

    added the welcome line

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome!"<<endl;
+    cout<<"This is a calculator designed to help you succeed in your Statics and Solids class."<<endl;
+
+
+}
